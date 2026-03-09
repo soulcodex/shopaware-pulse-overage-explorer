6aeba63b3c45cb2553f1913cc5fdc458d878c264
Magda Kokot <61601293+magdakok@users.noreply.github.com>
2026-03-09 10:56:29 +0000

    feat: add dashboard and shop details

---
+++ frontend/README.md
@@ -0,0 +1,2 @@
+# Project Thoughts
+1. 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import './style.css';
+import { MtDataTable } from '@shopware-ag/meteor-component-library';
 
-createApp(App).mount('#app');
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en: {},
+  },
+});
+
+const app = createApp(App);
+app.use(i18n);
+app.component('MtDataTable', MtDataTable);
+app.mount('#app');
